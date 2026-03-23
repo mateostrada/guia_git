fa4fcdc6bc1e06dd42b2d142c6880129b80f4a95
Mateo Strada <mateo.strada767@gmail.com>
2026-03-23 20:41:29 -0300

    resuelvo parte 1

---
+++ ejercicios_parte1.py
@@ -0,0 +1,36 @@
+# numero1=int(input("ingrese un num entere: "))
+# numero2=int(input("ingrese un num entere: "))
+# suma=numero1+numero2
+# resta=numero1-numero2
+# mult=numero1*numero2
+# divicion=numero1/numero2
+# print(suma)
+# print(resta)
+# print(mult)
+# print(divicion)
+
+#act 2
+# texto=input("ingrese su texto: ")
+# priml=texto[0:3]
+# ultl=texto[-3:]
+# concatenados=priml+ultl
+# print(f"{priml},      {ultl},         {concatenados}")
+
+
+#act3 ???
+# lista=[10,20,30,40,50]
+# print(lista)
+# lista[int(input("ingrese el lugar del numero(del 0 al 4): "))]=int(input("ingrese numero deseado: "))
+# print(f"lista nueva {lista}")
+
+# #act4
+# palabras_tupla = ("manzana", "pera", "uva", "naranja", "sandía", "manzana", "plátano", "kiwi", "pera", "fresa", "mango", "uva", "cereza", "manzana", "durazno")
+# valor=palabras_tupla[1]
+# cuenta=palabras_tupla.count("pera")
+# indice=palabras_tupla.index(valor,2)
+
+# print(f"{valor},    {cuenta},    {indice}")
+
+
+    
+
